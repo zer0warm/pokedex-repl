3ffcbf21d69de3f6caede19a467f8304987da7e5
zer0warm <dqhieu.2016@gmail.com>
2024-12-11 03:00:33 +0700

    Build a simple REPL
    - The prompt is Pokedex> (Much more readable than the lesson)
    - Strip whitespaces off input (blanks and tabs)
    - Split them with strings.Fields and select the first word

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex> ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		inputLine := strings.Trim(strings.ToLower(scanner.Text()), " \t")
+		command := strings.Fields(inputLine)[0]
+		fmt.Println("Your command was:", command)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
